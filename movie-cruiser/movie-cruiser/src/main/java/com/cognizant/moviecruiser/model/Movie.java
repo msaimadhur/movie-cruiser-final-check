ccae6c540050a0158e524d242a0646d530c54752
Sai Madhur <46928649+msaimadhur@users.noreply.github.com>
2021-03-23 10:06:43 +0530

    Update Movie.java

--- movie-cruiser/movie-cruiser/src/main/java/com/cognizant/moviecruiser/model/Movie.java
+++ movie-cruiser/movie-cruiser/src/main/java/com/cognizant/moviecruiser/model/Movie.java
@@ -103,7 +103,7 @@ public class Movie {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + (id ^ (id >>> 32));
 		return result;
 	}
 
